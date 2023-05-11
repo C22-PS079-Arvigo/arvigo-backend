91e9fd2e9ddf1b83243357c01b65a2dc62482519
Yusuf Wibisono <yusuf.wibisono@majoo.id>
2023-05-11 23:05:54 +0700

    update ddl, dml and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,20 @@
 # Use the official Golang image as the base image
-FROM golang:1.18-alpine AS build
+FROM golang:1.18-alpine
+
+# Maintainer info
+LABEL maintainer="Yusuf Wibisono <yusufw2429@gmail.com>"
 
 # Set the working directory inside the container
 WORKDIR /app
 
-# Copy the Go module files and download the dependencies
-COPY go.mod go.sum ./
-RUN go mod download
-
-# Copy the rest of the application source code
+# Copy the application source code to the container
 COPY . .
 
 # Build the Go application
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o arvigo ./cmd
-
-# Create a new minimal image for the runtime
-FROM alpine:latest
-
-# Set the working directory inside the container
-WORKDIR /app
-
-# Copy the built binary from the build stage
-COPY --from=build /app/arvigo .
+RUN go build -o main .
 
 # Expose the port on which the server will listen
 EXPOSE 8080
 
 # Run the Go application
-CMD ["./arvigo"]
+CMD ["./main"]
--- pkg/database/migration/003_seeders.sql
+++ pkg/database/migration/003_seeders.sql
@@ -6,11 +6,70 @@ VALUES
     ('oblong', 'face-shape', DEFAULT, DEFAULT),
     ('oval', 'face-shape', DEFAULT, DEFAULT),
     ('square', 'face-shape', DEFAULT, DEFAULT),
-    ('triangle', 'face-shape', DEFAULT, DEFAULT);
+    ('triangle', 'face-shape', DEFAULT, DEFAULT),
+    ('0', 'personality', DEFAULT, DEFAULT),
+    ('1', 'personality', DEFAULT, DEFAULT),
+    ('2', 'personality', DEFAULT, DEFAULT),
+    ('3', 'personality', DEFAULT, DEFAULT),
+    ('4', 'personality', DEFAULT, DEFAULT);
 
 -- auto-generated definition
-INSERT INTO role (name, created_at, updated_at)
+INSERT INTO roles (name, created_at, updated_at)
 VALUES 
     ('dashboard-app', DEFAULT, DEFAULT),
     ('user', DEFAULT, DEFAULT),
     ('merchant', DEFAULT, DEFAULT);
+
+-- auto-generated definition
+INSERT INTO personality_questionnaires (id, type, question, created_at, updated_at)
+VALUES
+    ('EXT', 'I am the life of the party', DEFAULT, DEFAULT),
+    ('EXT', 'I dont talk a lot', DEFAULT, DEFAULT),
+    ('EXT', 'I feel comfortable around people', DEFAULT, DEFAULT),
+    ('EXT', 'I keep in the background', DEFAULT, DEFAULT),
+    ('EXT', 'I start conversations', DEFAULT, DEFAULT),
+    ('EXT', 'I have little to say', DEFAULT, DEFAULT),
+    ('EXT', 'I talk to a lot of different people at parties', DEFAULT, DEFAULT),
+    ('EXT', 'I dont like to draw attention to myself', DEFAULT, DEFAULT),
+    ('EXT', 'I dont mind being the center of attention', DEFAULT, DEFAULT),
+    ('EXT', 'I am quiet around strangers', DEFAULT, DEFAULT),
+    ('EST', 'I get stressed out easily', DEFAULT, DEFAULT),
+    ('EST', 'I am relaxed most of the time', DEFAULT, DEFAULT),
+    ('EST', 'I worry about things', DEFAULT, DEFAULT),
+    ('EST', 'I seldom feel blue', DEFAULT, DEFAULT),
+    ('EST', 'I am easily disturbed', DEFAULT, DEFAULT),
+    ('EST', 'I get upset easily', DEFAULT, DEFAULT),
+    ('EST', 'I change my mood a lot', DEFAULT, DEFAULT),
+    ('EST', 'I have frequent mood swings', DEFAULT, DEFAULT),
+    ('EST', 'I get irritated easily', DEFAULT, DEFAULT),
+    ('EST', 'I often feel blue', DEFAULT, DEFAULT),
+    ('AGR', 'I feel little concern for others', DEFAULT, DEFAULT),
+    ('AGR', 'I am interested in people', DEFAULT, DEFAULT),
+    ('AGR', 'I insult people', DEFAULT, DEFAULT),
+    ('AGR', 'I sympathize with others feelings', DEFAULT, DEFAULT),
+    ('AGR', 'I am not interested in other peoples problems', DEFAULT, DEFAULT),
+    ('AGR', 'I have a soft heart', DEFAULT, DEFAULT),
+    ('AGR', 'I am not really interested in others', DEFAULT, DEFAULT),
+    ('AGR', 'I take time out for others', DEFAULT, DEFAULT),
+    ('AGR', 'I feel others emotions', DEFAULT, DEFAULT),
+    ('AGR', 'I make people feel at ease', DEFAULT, DEFAULT),
+    ('CSN', 'I am always prepared', DEFAULT, DEFAULT),
+    ('CSN', 'I leave my belongings around', DEFAULT, DEFAULT),
+    ('CSN', 'I pay attention to details', DEFAULT, DEFAULT),
+    ('CSN', 'I make a mess of things', DEFAULT, DEFAULT),
+    ('CSN', 'I get chores done right away', DEFAULT, DEFAULT),
+    ('CSN', 'I often forget to put things back in their proper place', DEFAULT, DEFAULT),
+    ('CSN', 'I like order', DEFAULT, DEFAULT),
+    ('CSN', 'I shirk my duties', DEFAULT, DEFAULT),
+    ('CSN', 'I follow a schedule', DEFAULT, DEFAULT),
+    ('CSN', 'I am exacting in my work', DEFAULT, DEFAULT),
+    ('OPN', 'I have a rich vocabulary', DEFAULT, DEFAULT),
+    ('OPN', 'I have difficulty understanding abstract ideas', DEFAULT, DEFAULT),
+    ('OPN', 'I have a vivid imagination', DEFAULT, DEFAULT),
+    ('OPN', 'I am not interested in abstract ideas', DEFAULT, DEFAULT),
+    ('OPN', 'I have excellent ideas', DEFAULT, DEFAULT),
+    ('OPN', 'I do not have a good imagination', DEFAULT, DEFAULT),
+    ('OPN', 'I am quick to understand things', DEFAULT, DEFAULT),
+    ('OPN', 'I use difficult words', DEFAULT, DEFAULT),
+    ('OPN', 'I spend time reflecting on things', DEFAULT, DEFAULT),
+    ('OPN', 'I am full of ideas', DEFAULT, DEFAULT);
