20a4a7cc2f09df645da820e46c34b65f221b4851
Yusuf Wibisono <yusuf.wibisono@majoo.id>
2023-05-18 06:56:16 +0700

    add face shape struct db

---
+++ datastruct/face_shape.go
@@ -0,0 +1,29 @@
+package datastruct
+
+import "time"
+
+type (
+	FaceShape struct {
+		ID        uint64    `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+		Name      string    `gorm:"column:name" json:"name"`
+		CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+		UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
+	}
+
+	DetailFaceShapeTag struct {
+		ID          uint64    `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+		Name        string    `gorm:"column:name" json:"name"`
+		FaceShapeID uint64    `gorm:"column:face_shape_id" json:"face_shape_id"`
+		TagID       uint64    `gorm:"column:tag_id" json:"tag_id"`
+		CreatedAt   time.Time `gorm:"column:created_at" json:"created_at"`
+		UpdatedAt   time.Time `gorm:"column:updated_at" json:"updated_at"`
+	}
+)
+
+func (FaceShape) TableName() string {
+	return "face_shape"
+}
+
+func (DetailFaceShapeTag) TableName() string {
+	return "detail_face_shape_tags"
+}
