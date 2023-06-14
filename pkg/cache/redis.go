777fa2a9bc2743e26bb36bada3c5f4bb99d27e96
Yusuf Wibisono <yusuf.wibisono@majoo.id>
2023-06-15 02:17:01 +0700

    add env example

---
+++ pkg/cache/redis.go
@@ -0,0 +1,42 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/yusufwib/arvigo-backend/utils"
+)
+
+func ConnectRedis() (*redis.Client, error) {
+	var (
+		redisHost     = os.Getenv("REDIS_HOST")
+		redisPort     = os.Getenv("REDIS_PORT")
+		redisUsername = os.Getenv("REDIS_USERNAME")
+		redisPassword = os.Getenv("REDIS_PASSWORD")
+		redisDB       = os.Getenv("REDIS_DB")
+	)
+
+	// Create a Redis client
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisHost + ":" + redisPort,
+		Username: redisUsername,
+		Password: redisPassword,
+		DB:       utils.StrToInt(redisDB, 0),
+		// TLSConfig: &tls.Config{
+		// 	InsecureSkipVerify: true,
+		// },
+	})
+
+	// Test Connection And Auth with PING
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	if err := client.Ping(ctx).Err(); err != nil {
+		return nil, fmt.Errorf("ping command: %w", err)
+	}
+
+	return client, nil
+}
